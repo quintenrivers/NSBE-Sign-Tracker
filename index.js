bcc560ec5d3125bbd42faed8630e3704c601dc0a
quintenrivers <32085810+quintenrivers@users.noreply.github.com>
2018-09-01 19:55:32 -0400

    minor changes to UI

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // client-side javascript file
 function initMap()
 {
-	var options =
-	{
-		zoom: 18, 
-		center:{lat:29.6484,lng:-82.3485}
-	}
+	var weil = {lat:29.6483,lng:-82.3485};
 
-	var map = new google.maps.Map(document.getElementById('map'), options);
+	var map = new google.maps.Map(document.getElementById('map'), {zoom:18,center:weil});
+	var marker = new google.maps.Marker({position:weil,map:map});
 }
