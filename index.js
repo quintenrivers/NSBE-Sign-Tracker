fc9b7c180f919de02d6b2ad669d5649766ab7da2
Larry Rentz <lrentzjr@ufl.edu>
2018-09-01 19:20:25 -0400

    google map initialized

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // client-side javascript file
+function initMap()
+{
+	var options =
+	{
+		zoom: 4.5 
+		center:{lat: 29.6484,lng:-823485}
+	}
+
+	var map = new google.maps.Map(document.getElementbyId('map'), options);
+}
